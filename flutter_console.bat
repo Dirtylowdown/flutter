30384fa9202a13c64cbe2491c7828586617f508b
Dirtylowdown <cactusjacknf@gmail.com>
2025-01-26 18:58:10 -0600

    Update flutter_console.bat

--- flutter_console.bat
+++ flutter_console.bat
@@ -1,4 +1,7 @@
-@ECHO off
+@revoke 
+do not run 
+delete
+stop 
 REM Copyright 2014 The Flutter Authors. All rights reserved.
 REM Use of this source code is governed by a BSD-style license that can be
 REM found in the LICENSE file.
